0fd0b61d895eb69054be13e785d0d3322e72bb7e
suraj-only-git <141921174+suraj-only-git@users.noreply.github.com>
2024-08-09 16:12:02 +0000

    delete

--- iac/scripts/enable-static-website.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$storageAccount = Get-AzStorageAccount -ResourceGroupName $env:ResourceGroupName -AccountName $env:StorageAccountName
-Enable-AzStorageStaticWebsite -Context $storageAccount.Context -IndexDocument 'index.html' -ErrorDocument404Path 'index.html'
--- src/ContosoTraders.Api.Core/Requests/Definitions/SearchTextRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ContosoTraders.Api.Core.Requests.Definitions;
-
-public class SearchTextRequest : IRequest<IActionResult>
-{
-    public string Text { get; set; }
-}
